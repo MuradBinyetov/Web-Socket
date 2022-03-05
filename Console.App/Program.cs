c1acb1f6304ebc2469a4dbd0f587277e29a3e9f7
MuradBinyetov <murad_binyetov@mail.ru>
2022-03-05 11:34:20 +0400

    Initial Socket

---
+++ Console.App/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Socket_Server
+{
+    class Program
+    {
+        private static byte[] _buffer = new byte[1024];
+        private static  Socket _serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private static List<Socket> _clientSockets = new List<Socket>();
+        static void Main(string[] args)
+        {
+            SetupServer();
+            Console.ReadLine();
+        }
+        static void SetupServer()
+        {
+            Console.WriteLine("setting up server");
+
+            _serverSocket.Bind(new IPEndPoint(IPAddress.Any, 100));
+
+            _serverSocket.Listen(1);
+            _serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+
+        }
+        static void AcceptCallback(IAsyncResult AR)
+        {
+            Socket socket = _serverSocket.EndAccept(AR);
+            _clientSockets.Add(socket);
+            Console.WriteLine("new client connection established");
+
+            socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+            _serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+        }
+        static void ReceiveCallback(IAsyncResult AR)
+        {
+            Socket socket = (Socket)AR.AsyncState;
+            int received = socket.EndReceive(AR);
+            byte[] databuf = new byte[received];
+            Array.Copy(_buffer, databuf, received);
+            string text = Encoding.ASCII.GetString(databuf);
+            Console.WriteLine("Text received:" + text);
+            string response = DateTime.Now.ToString();
+            byte[] data = Encoding.ASCII.GetBytes(response);
+            socket.BeginSend(data, 0,data.Length, SocketFlags.None, new AsyncCallback(SendCallback), socket);
+            socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+        }
+        static void SendCallback(IAsyncResult AR)
+        {
+            Socket socket = (Socket)AR.AsyncState;
+            socket.EndSend(AR);
+        }
+    }
+
+
+}
